114cddd7588c508aa0c45e37d9d5c67969180f48
Mauro <mauroadauto9@gmail.com>
2024-05-01 17:35:29 -0500

    ADDED | Entidades completas

--- orientaTest/src/main/java/com/its/orientaTest/model/entities/Categoria.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/entities/Categoria.java
@@ -1,5 +1,20 @@
 package com.its.orientaTest.model.entities;
 
-public class Categoria {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
 }
--- orientaTest/src/main/java/com/its/orientaTest/model/entities/Respuesta.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/entities/Respuesta.java
@@ -1,5 +1,24 @@
 package com.its.orientaTest.model.entities;
 
-public class Respuesta {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "respuesta")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Respuesta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "valor", nullable = false)
+    private Integer valor;
+
+    @ManyToOne
+    @JoinColumn(name = "pregunta_id", nullable = false)
+    private Pregunta pregunta;
 }
