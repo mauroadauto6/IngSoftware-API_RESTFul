c11059c87ac9d4dd86f0670b1882f013efd88676
Jair Rodriguez <jair1232@hotmail.com>
2024-05-01 12:08:52 -0500

    ADDED/UPDATE | DTO Estudiante/Carrera/Carrera_Universidad

---
+++ orientaTest/src/main/java/com/its/orientaTest/model/dto/Carrera_UniversidadResponseDTO.java
@@ -0,0 +1,15 @@
+package com.its.orientaTest.model.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Carrera_UniversidadResponseDTO {
+    private Long Id;
+    private CarreraResponseDTO carrera;
+    private UniversidadResponseDTO universidad;
+}
--- orientaTest/src/main/java/com/its/orientaTest/model/dto/EstudianteRequestDTO.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/dto/EstudianteRequestDTO.java
@@ -1,5 +1,33 @@
 package com.its.orientaTest.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class EstudianteRequestDTO {
-    
+
+    @NotBlank(message = "El nombre de estudiante no puede estar vacio")
+    private String nombre;
+
+    @NotBlank(message = "Los apellido de estudiante no puede estar vacio")
+    private String apellido;
+
+    @NotBlank(message = "El correo de estudiante no puede estar vacio")
+    @Email
+    private String correoElectronico;
+
+    @NotBlank(message = "El telefono de estudiante no puede estar vacio")
+    @Pattern(regexp = "[0-9]+", message = "El numero de telefono solo puede contener digitos")
+    private String telefono;
+
+    @NotBlank(message = "La direccion de estudiante no puede estar vacio")
+    private String direccion;
 }
