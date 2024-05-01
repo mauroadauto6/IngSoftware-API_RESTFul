ee5bf39cd29dd966dad4d345104a19499d51179d
Jair Rodriguez <jair1232@hotmail.com>
2024-05-01 13:15:42 -0500

    ADDED | Mapper

--- orientaTest/src/main/java/com/its/orientaTest/model/dto/CarreraResponseDTO.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/dto/CarreraResponseDTO.java
@@ -1,5 +1,14 @@
 package com.its.orientaTest.model.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarreraResponseDTO {
-    
+    private String nombre;
+    private String descripcion;
 }
