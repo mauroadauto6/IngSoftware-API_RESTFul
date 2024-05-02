7ebd8019181c793b297a694aff2e20ffd7df6c9a
David Niño <daavidninoosuaarez@gmail.com>
2024-05-02 11:19:54 -0500

    UPDATE | Test, DTO

--- orientaTest/src/main/java/com/its/orientaTest/model/dto/TestResponseDTO.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/dto/TestResponseDTO.java
@@ -1,5 +1,15 @@
 package com.its.orientaTest.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TestResponseDTO {
-    
+    private String tipoTest;
+    private LocalDate fechaTest;
+    private List<PreguntaResponseDTO> preguntas;
 }
--- orientaTest/src/main/java/com/its/orientaTest/model/entities/Test.java
+++ orientaTest/src/main/java/com/its/orientaTest/model/entities/Test.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "test")
@@ -26,4 +28,12 @@ public class Test {
     @ManyToOne
     @JoinColumn(name = "estudiante_id", nullable = false)
     private Estudiante estudiante;
+
+    @ManyToMany
+    @JoinTable(
+        name = "test_pregunta",
+        joinColumns = @JoinColumn(name = "test_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "pregunta_id", referencedColumnName = "id")
+    )
+    private List<Pregunta> preguntas = new ArrayList<>();
 }
